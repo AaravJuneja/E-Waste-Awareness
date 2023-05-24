83bb95270034965f345d762e9b9597f484b06a0f
Aarav Juneja (He/Him) <aarav.juneja@ais.amity.edu>
2023-05-24 21:21:56 +0530

    First Commit

--- script.js
+++ script.js
@@ -16,3 +16,38 @@ function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
+
+function smoothScroll(target, duration) {
+  const targetElement = document.querySelector(target);
+  const targetPosition = targetElement.offsetTop;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const scrollY = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, scrollY);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const target = e.target.getAttribute('href');
+    smoothScroll(target, 500);
+  });
+});
