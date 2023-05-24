81a346c76b1aa23faf24a63ac665b358540c7501
Aarav Juneja (He/Him) <aarav.juneja@ais.amity.edu>
2023-05-24 17:56:15 +0530

    First Commit

--- script.js
+++ script.js
@@ -31,7 +31,43 @@ navLinks.forEach(function (link) {
   link.addEventListener('click', function (e) {
     e.preventDefault();
     var target = this.getAttribute('href');
-    var duration = 1000; // Adjust the scroll duration as needed
+    var duration = 1000;
     smoothScroll(target, duration);
   });
+});
+
+
+// Scroll to top
+function scrollToTop() {
+  var duration = 1000;
+  var start = window.pageYOffset;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, start, -start, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  // Easing function
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+// Show or hide the scroll-to-top button based on the scroll position
+var scrollBtn = document.querySelector('.scroll-to-top-btn');
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 200) {
+    scrollBtn.classList.add('show');
+  } else {
+    scrollBtn.classList.remove('show');
+  }
 });
